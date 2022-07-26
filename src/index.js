e173894e2f40f9f8788c7243ab83cb0cd894e019
Julien Hnery <jwh_917@yahoo.com>
2022-07-25 22:43:09 -0400

    Updated Components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
   );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
